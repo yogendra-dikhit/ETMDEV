48cef4a2ba0799aa8be3231e2601d3b5e15e6a01
yogendra-dikhit <yogendradikhit1996@gmail.com>
2020-01-23 23:27:23 +0530

    m

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-manager',
-  template: `
-    <p>
-      manager works!
-    </p>
-  `,
-  styles: []
+  template: 'manager.component.html',
+  styles: ['manager.component.css']
 })
 export class ManagerComponent implements OnInit {
 
